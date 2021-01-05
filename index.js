b2ee84d126129adfb410d9d15ae2fdd2b819302d
RevinfotechBlockchain <manish.gaur@revinfotech.com>
2021-01-05 17:35:21 +0530

    cors added before api call

--- index.js
+++ index.js
@@ -18,8 +18,6 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-app.use('/api/eth/nexon', route);
 app.use(cors());
 app.options('*', cors());
 
@@ -28,6 +26,9 @@ app.use((req, res, next) => {
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   next();
 });
+app.use(express.static(path.join(__dirname, 'public')));
+app.use('/api/eth/nexon', route);
+
 
 mongoose.connect(DB, {
   useNewUrlParser: true,
